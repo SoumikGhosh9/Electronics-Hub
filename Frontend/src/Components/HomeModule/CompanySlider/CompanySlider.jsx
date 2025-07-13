d12314692819cdc0a79218546190a0b2f75398c4
Soumik Ghosh <soumikghosh09072004@gmail.com>
2025-07-14 00:05:23 +0530

    Hompage Fixed

--- Frontend/src/Components/HomeModule/CompanySlider/CompanySlider.jsx
+++ Frontend/src/Components/HomeModule/CompanySlider/CompanySlider.jsx
@@ -1,15 +1,15 @@
 import styles from "./CompanySlider.module.css";
 import haier from "../../../assets/haier.png";
-import bluestar from "../../../Assets/bluestar.png";
-import daikin from "../../../Assets/daikin.png";
-import god from "../../../Assets/god.png";
-import whirlpool from "../../../Assets/whirlpool.png";
-import bajaj from "../../../Assets/bajaj.png";
-import havells from "../../../Assets/havells.png";
-import voltasll from "../../../Assets/voltasll.png";
-import hitachi from "../../../Assets/hitachi.png";
-import philips from "../../../Assets/philips.png";
-import boat from "../../../Assets/boat.png";
+import bluestar from "../../../assets/bluestar.png";
+import daikin from "../../../assets/daikin.png";
+import god from "../../../assets/god.png";
+import whirlpool from "../../../assets/whirlpool.png";
+import bajaj from "../../../assets/bajaj.png";
+import havells from "../../../assets/havells.png";
+import voltasll from "../../../assets/voltasll.png";
+import hitachi from "../../../assets/hitachi.png";
+import philips from "../../../assets/philips.png";
+import boat from "../../../assets/boat.png";
 const CompanySlider = () => {
     const images = [haier, bluestar, daikin, god, whirlpool, bajaj, havells];
 
